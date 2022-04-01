b814916849903a251e3f89b31d2575c178e47ca1
Oleksiy Bolgarov <oleksiy.bolgarov@hop-child.com>
2022-04-01 17:28:07 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Arr, me matey'));
+app.get('/', (req, res) => res.send('Yo mama so old I told her to act her age and she died'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
